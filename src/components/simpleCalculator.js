998aa167361a6cb268cea3bcc5db40214d9ce240
mukhammadrizooff <legend.it@bk.ru>
2022-02-24 20:19:38 +0500

    Fix linters

--- src/components/simpleCalculator.js
+++ src/components/simpleCalculator.js
@@ -17,36 +17,36 @@ const Calculator = () => {
     setProperties(calculate(newProperties, funcBtn));
   };
 
-    return (
-      <>
-        <div className="box-container">
-          <div className="result-screen">
-            { properties.total}
-            { properties.operation }
-            { properties.next }
-        </div>
-          <button type="button" onClick={handleClick} className="btn">AC</button>
-          <button type="button" onClick={handleClick} className="btn">+/-</button>
-          <button type="button" onClick={handleClick} className="btn">%</button>
-          <button type="button" onClick={handleClick} className="btn hover-btn">÷</button>
-          <button type="button" onClick={handleClick} className="btn">7</button>
-          <button type="button" onClick={handleClick} className="btn">8</button>
-          <button type="button" onClick={handleClick} className="btn">9</button>
-          <button type="button" onClick={handleClick} className="btn hover-btn">x</button>
-          <button type="button" onClick={handleClick} className="btn">4</button>
-          <button type="button" onClick={handleClick} className="btn">5</button>
-          <button type="button" onClick={handleClick} className="btn">6</button>
-          <button type="button" onClick={handleClick} className="btn hover-btn">-</button>
-          <button type="button" onClick={handleClick} className="btn">1</button>
-          <button type="button" onClick={handleClick} className="btn">2</button>
-          <button type="button" onClick={handleClick} className="btn">3</button>
-          <button type="button" onClick={handleClick} className="btn hover-btn">+</button>
-          <button type="button" onClick={handleClick} className="btn screen-num">0</button>
-          <button type="button" onClick={handleClick} className="btn">.</button>
-          <button type="button" onClick={handleClick} className="btn hover-btn">=</button>
-        </div>
-      </>
-    );
-  };
+  return (
+    <>
+      <div className="box-container">
+        <div className="result-screen">
+          { properties.total}
+          { properties.operation }
+          { properties.next }
+      </div>
+        <button type="button" onClick={handleClick} className="btn">AC</button>
+        <button type="button" onClick={handleClick} className="btn">+/-</button>
+        <button type="button" onClick={handleClick} className="btn">%</button>
+        <button type="button" onClick={handleClick} className="btn hover-btn">÷</button>
+        <button type="button" onClick={handleClick} className="btn">7</button>
+        <button type="button" onClick={handleClick} className="btn">8</button>
+        <button type="button" onClick={handleClick} className="btn">9</button>
+        <button type="button" onClick={handleClick} className="btn hover-btn">x</button>
+        <button type="button" onClick={handleClick} className="btn">4</button>
+        <button type="button" onClick={handleClick} className="btn">5</button>
+        <button type="button" onClick={handleClick} className="btn">6</button>
+        <button type="button" onClick={handleClick} className="btn hover-btn">-</button>
+        <button type="button" onClick={handleClick} className="btn">1</button>
+        <button type="button" onClick={handleClick} className="btn">2</button>
+        <button type="button" onClick={handleClick} className="btn">3</button>
+        <button type="button" onClick={handleClick} className="btn hover-btn">+</button>
+        <button type="button" onClick={handleClick} className="btn screen-num">0</button>
+        <button type="button" onClick={handleClick} className="btn">.</button>
+        <button type="button" onClick={handleClick} className="btn hover-btn">=</button>
+      </div>
+    </>
+  );
+};
 
 export default Calculator;
